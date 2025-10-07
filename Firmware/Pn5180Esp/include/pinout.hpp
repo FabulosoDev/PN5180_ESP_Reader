d33920ab342fbf5367005a33294ddc1b3be1ddd1
FabulosoDev <113933725+FabulosoDev@users.noreply.github.com>
2025-10-07 15:34:40 +0200

    - use board specific pinout

--- Firmware/Pn5180Esp/include/pinout.hpp
+++ Firmware/Pn5180Esp/include/pinout.hpp
@@ -1,21 +1,22 @@
 #pragma once
 
-#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560) || defined(ARDUINO_AVR_NANO)
-    #define PIN_PN5180_NSS  10
-    #define PIN_PN5180_BUSY 9
-    #define PIN_PN5180_RST  7
+#if defined(BOARD_D1_MINI)
+    #define PIN_PN5180_NSS  4
+    #define PIN_PN5180_BUSY 16
+    #define PIN_PN5180_RST  5
+    #define PIN_WS2812B    D8
 
-#elif defined(ARDUINO_ARCH_ESP32)
+#elif defined(BOARD_D1_MINI_ESP32)
     #define PIN_PN5180_NSS  21
     #define PIN_PN5180_BUSY 26
     #define PIN_PN5180_RST  22
     #define PIN_WS2812B     15
 
-#elif defined(ARDUINO_ARCH_ESP8266)
-    #define PIN_PN5180_NSS  4
-    #define PIN_PN5180_BUSY 16
-    #define PIN_PN5180_RST  5
-    #define PIN_WS2812B    D8
+#elif defined(BOARD_ESP32C3_SUPERMINI)
+    #define PIN_PN5180_NSS  0
+    #define PIN_PN5180_BUSY 1
+    #define PIN_PN5180_RST  10
+    #define PIN_WS2812B     3
 
 #else
     #error Please define your pinout here!
